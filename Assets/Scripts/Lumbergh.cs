bfb9c40bb568d388e6f30855b7243d961c2e385e
Topher Wilson <tophermade@gmail.com>
2017-05-27 15:11:10 -0600

    improving gameboard layout and adding lumbergh

---
+++ Assets/Scripts/Lumbergh.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lumbergh : MonoBehaviour {
+
+	// prefab references
+	public GameObject[] boards;
+	public GameObject[] trees;
+	public GameObject[] bushes;
+
+	public GameObject[] obstacles;
+
+
+	// object references
+	public GameObject player;
+	public GameObject spawnParent;
+
+
+	// bools
+	public bool playing = false;
+
+
+	// numerical
+	public int boardLength = 30;
+	public int lastBoardAt = 0;
+	public int currentScore = 0;
+
+	public float leftLane = 9.0f;
+	public float rightLane = 5.0f;
+
+
+
+	void AddBoardToGame(){
+		lastBoardAt += boardLength;
+	}
+
+
+	void ManageBoard(){
+		if(player.transform.position.x > lastBoardAt - (boardLength *2)){
+			AddBoardToGame();
+		}
+	}
+
+}
