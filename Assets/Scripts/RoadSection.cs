f110a8c200aae65bb7f6ac054e68dcb1ac6c203e
Topher Wilson <tophermade@gmail.com>
2017-05-27 20:08:46 -0600

    rm stupid prefabs, merged objects, spawning decore. yay.

---
+++ Assets/Scripts/RoadSection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSection : MonoBehaviour {
+
+	public GameObject[] decorSpawns;
+	public GameObject[] obstacleSpawns;
+
+	public GameObject[] decorPrefabs;
+	public GameObject[] obstaclePrefabs;
+
+	void SpawnDecor(){
+		foreach(GameObject spawn in decorSpawns){
+			if(Random.Range(1,6) == 3){
+				GameObject newDecor = Instantiate(decorPrefabs[Random.Range(0,decorPrefabs.Length)], spawn.transform.position, Quaternion.identity);
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		SpawnDecor();
+	}
+	
+}
